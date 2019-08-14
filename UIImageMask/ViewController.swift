c914eb36221b2ceba499231b0ec20ed91c43cb54
mohamedosama2074 <iosengks74@gmail.com>
2019-08-14 22:08:09 +0200

    ImageRounded

--- UIImageMask/ViewController.swift
+++ UIImageMask/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageview: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
+        // make image rounded
+        
+        imageview.layer.cornerRadius = imageview.frame.size.width / 2
+        imageview.clipsToBounds = true
+        imageview.layer.shouldRasterize = true
+        imageview.layer.borderColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        imageview.layer.borderWidth = 3
     }
 
 
